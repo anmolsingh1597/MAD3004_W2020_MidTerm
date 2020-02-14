4740997dc5eb905f7304ff64a40754cb453be92d
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-13 19:08:12 -0500

    [Anmol Singh] Class Bill: Initialized

---
+++ CO777245_W2020_MID_TERM_MAD3004/CO777245_W2020_MID_TERM_MAD3004/Bill.swift
@@ -0,0 +1,19 @@
+//
+//  Bill.swift
+//  CO777245_W2020_MID_TERM_MAD3004
+//
+//  Created by Anmol singh on 2020-02-13.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+/* Create Bill class containing following attributes with appropriate data types, initializer and methods to manipulate data.
+-Bill id, Bill Date, Bill Type and Total Bill Amount */
+
+import Foundation
+
+public class Bill
+{
+    let billId: Int
+    var billDate: Date
+    var billType:
+}

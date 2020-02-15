2fb1097d68fe601e13baa4ba989cb331403ae1c3
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-15 16:47:51 -0500

    [Anmol Singh]Class Hydro: Intialized

---
+++ CO777245_W2020_MID_TERM_MAD3004/CO777245_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,38 @@
+//
+//  Hydro.swift
+//  CO777245_W2020_MID_TERM_MAD3004
+//
+//  Created by Anmol singh on 2020-02-15.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+/*Create Hydro class which inherits Bill class and containing following attributes with appropriate data types, initializer and methods to manipulate data.
+- Agency Name, Unit consumed*/
+
+import Foundation
+
+
+public class Hydro: Bill
+{
+    var agencyName: String
+    var unitConsumed: Double
+    
+    init(billId: Int, billDate: Date, agencyName: String, unitConsumed: Double){
+        self.agencyName = agencyName
+        self.unitConsumed = unitConsumed
+        super.init(billId: billId, billDate: billDate, billType: Enum.BillType.Hydro)
+    }
+    
+   override func calculateBill() -> Double{
+        
+    super.billAmount = 1 * self.unitConsumed
+        return super.billAmount
+    }
+    
+    override func display() {
+        print("Bill ID: \(self.billId.concatInternetPrefix())")
+        super.display()
+        print("Provider Name: \(self.agencyName)")
+        print("Internet GB Used: \(self.unitConsumed) units")
+    }
+    
+}

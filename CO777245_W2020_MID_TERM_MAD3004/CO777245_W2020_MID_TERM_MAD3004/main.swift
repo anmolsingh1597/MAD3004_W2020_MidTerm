4a1da472095d70f4af28fa209b3e80fc54acce96
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-16 22:19:06 -0500

    [Anmol Singh] Testing: Output values formatted

--- CO777245_W2020_MID_TERM_MAD3004/CO777245_W2020_MID_TERM_MAD3004/main.swift
+++ CO777245_W2020_MID_TERM_MAD3004/CO777245_W2020_MID_TERM_MAD3004/main.swift
@@ -22,9 +22,9 @@ var hBill1 = Hydro(billId: 5, billDate: Date(), agencyName: "Canadian Supply", u
 
 var hBill2 = Hydro(billId: 6, billDate: Date.from(year: 2020, month: 01, day: 18) ?? Date(), agencyName: "Planet Energy", unitConsumed: 45)
 
-var c1 : Customer = Customer(id: 1, firstName: "Anmol", lastName: "Singh", emailId: "me_anmol@me.com", billarray: [mBill2, iBill1])
+var c1 : Customer = Customer(id: 1, firstName: "Anmol", lastName: "Singh", emailId: "me_anmol@me.com", billarray: [mBill1, iBill1])
 
-var c2 : Customer = Customer(id: 2, firstName: "Aman", lastName: "Kaur", emailId: "me_aman@me.com", billarray: [mBill1, iBill2, hBill1])
+var c2 : Customer = Customer(id: 2, firstName: "Aman", lastName: "Kaur", emailId: "me_aman@me.com", billarray: [mBill2, iBill2, hBill1])
 
 var c3 : Customer = Customer(id: 3, firstName: "Ikroop", lastName: "Virk", emailId: "me_ikroop@me.com", billarray: [hBill2])
 
